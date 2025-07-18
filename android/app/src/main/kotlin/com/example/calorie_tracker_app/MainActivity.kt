f139ffe43420cf1d0607316a361f5ce0c85cba2e
911samuel <abayizeraeaz@gmail.com>
2025-07-18 09:54:56 +0200

    fix: Updated package declaration and removed unnecessary import in MainActivity.kt

--- android/app/src/main/kotlin/com/example/calorie_tracker_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/calorie_tracker_app/MainActivity.kt
@@ -1,5 +1,5 @@
-flutter pub add test_cov_consolepackage com.example.calorie_tracker_app
+package com.example.calorie_tracker_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {}
